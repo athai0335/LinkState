d2014abe7b12460e8a1a37713d74707dd5a7aa22
Baradji <ladjimali@gmail.com>
2017-11-06 17:46:41 -0700

    Create router.h

---
+++ router.h
@@ -0,0 +1,46 @@
+#ifndef ROUTER_H_INCLUDED
+#define ROUTER_H_INCLUDED
+
+#include <stdio.h>
+#include <strings.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <getopt.h>
+#include<stdbool.h>
+
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+
+
+class router {
+	public:
+
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif
